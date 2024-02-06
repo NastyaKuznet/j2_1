59e6af8a0bb0a99eb908b09400a4135672ce2f74
Anastasiya Kuznetsova <115180166+NastyaKuznet@users.noreply.github.com>
2024-02-06 12:22:39 +0500

    create controller

---
+++ src/main/java/com/example/demo/controller/PostsViewController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostsViewController {
+    @ResponseBody
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String list() {
+        return "Здесь будет главная страница";
+    }
+}
