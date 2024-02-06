54ca5a0e2f7467d406aedd92d596ebfe107db210
Anastasiya Kuznetsova <115180166+NastyaKuznet@users.noreply.github.com>
2024-02-06 12:24:04 +0500

    in controller there is method for post

--- src/main/java/com/example/demo/controller/PostsViewController.java
+++ src/main/java/com/example/demo/controller/PostsViewController.java
@@ -9,4 +9,10 @@ public class PostsViewController {
     public String list() {
         return "Здесь будет главная страница";
     }
+
+    @ResponseBody
+    @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
+    public String single(@PathVariable("id") Long id) {
+        return "Здесь будет страница поста №" + id;
+    }
 }
