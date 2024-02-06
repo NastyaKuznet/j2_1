eb6e378fe29d43e1d4a8a518b3cbbbd7fc4a2f0a
Anastasiya Kuznetsova <115180166+NastyaKuznet@users.noreply.github.com>
2024-02-06 13:00:12 +0500

    model and posts

--- src/main/java/com/example/demo/controller/PostsViewController.java
+++ src/main/java/com/example/demo/controller/PostsViewController.java
@@ -1,16 +1,21 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.service.PostService;
 import org.springframework.stereotype.*;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.*;
 
 @Controller
 public class PostsViewController {
+    @Autowired
+    PostService postsService;
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String list(Model model) {
         model.addAttribute("appName", "Моё супер приложение");
+        model.addAttribute("posts", postsService.listAllPosts());
         return "list";
     }
 
